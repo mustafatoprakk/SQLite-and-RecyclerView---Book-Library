6b72500a71e73d5adb0165e0ab309c5d3b407e76
mustafatoprakk <mmustafa.ttoprak@gmail.com>
2021-01-02 16:09:59 +0300

    SQLite and recyclerview

---
+++ app/src/main/java/com/example/sqlitebooklibraryandrecyclerview/AddActivity.java
@@ -0,0 +1,35 @@
+package com.example.sqlitebooklibraryandrecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText title_input, author_input, pages_input;
+    private Button save_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        title_input = findViewById(R.id.title_input);
+        author_input = findViewById(R.id.author_input);
+        pages_input = findViewById(R.id.pages_input);
+        save_button = findViewById(R.id.save_button);
+
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+                myDB.addBook(title_input.getText().toString().trim(), author_input.getText().toString().trim(),
+                        Integer.valueOf(pages_input.getText().toString().trim()));
+            }
+        });
+    }
+}
